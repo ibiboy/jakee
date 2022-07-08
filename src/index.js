99096306e6fa2149c4052218676ee59b083a06ca
ibiboy <ibichaninter7@gmail.com>
2022-07-08 22:47:13 +0900

    letとconst

--- src/index.js
+++ src/index.js
@@ -1,10 +1,35 @@
-import "./styles.css";
+// var val1 = "var変数";
+// console.log(val1);
 
-document.getElementById("app").innerHTML = `
-<h1>Hello Vanilla!</h1>
-<div>
-  We use the same configuration as Parcel to bundle this sandbox, you can find more
-  info about Parcel 
-  <a href="https://parceljs.org" target="_blank" rel="noopener noreferrer">here</a>.
-</div>
-`;
+// val1 = "上書き";
+// console.log(val1);
+
+// var val1 = "var変数";
+// console.log(val1);
+
+// let val2 = "let変数";
+// console.log(val2);
+
+// val2 = "let変数上書き";
+// console.log(val2);
+
+// let val2 = "let変数";
+
+// const var3 = "const変数";
+// console.log(var3);
+
+// var3 = "const上書き";
+
+// const var3 = "const変数";
+
+// const val4 = {
+//   name: "ジャケえ",
+//   age: 28
+// };
+// val4.name = "jak";
+// console.log(val4.name);
+
+const val5 = ["dog", "cat"];
+val5[0] = "bird";
+val5.push("monkey");
+console.log(val5);
